9a15a5990dc1b6af72b707b9c8ff97cd20047be9
elifober <eliasfobert@gmail.com>
2026-04-06 16:51:40 -0600

    Add Dockerfile

--- backend/Beacon.API/Program.cs
+++ backend/Beacon.API/Program.cs
@@ -20,7 +20,10 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthorization();
 
